c04f16dce7264a60750597cb17d6be717e96d0c5
just4programming <sergey@bitfi.com>
2021-11-30 11:34:58 +0400

    updated the trained model

--- nft-predictor/entrypoint.sh
+++ nft-predictor/entrypoint.sh
@@ -4,7 +4,7 @@ WEIGHTS_PATH="$PWD/data/6_class_4_layer_100epoch.h5"
 if [ -f "$WEIGHTS_PATH" ]; then
   echo "$WEIGHTS_PATH exists"
 else 
-  ID="13tvy9SufwulPw3C0gJAvfyoYJAVrOv80"
+  ID="17tq47-ogip3DSfZxsqhrr80Vh4HeBQNP"
   URL="https://docs.google.com/uc?export=download&id=$ID"
   wget --load-cookies /tmp/cookies.txt \
     "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$ID" \
